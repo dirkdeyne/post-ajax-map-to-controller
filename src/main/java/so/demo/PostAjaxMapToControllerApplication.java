3921889c196b664878bc4e99842f4b4412c9dbe1
dirkdeyne <dirkdeyne@gmail.com>
2019-10-21 19:25:14 +0200

    updated answer

--- src/main/java/so/demo/PostAjaxMapToControllerApplication.java
+++ src/main/java/so/demo/PostAjaxMapToControllerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -23,8 +24,10 @@ public class PostAjaxMapToControllerApplication {
     }
 
     @PostMapping("/post")
-    public void post(@RequestBody Map<String,String> data) {
+    @ResponseBody
+    public String post(@RequestBody Map<String,String> data) {
         System.out.println(data);
+        return "data well received";
     }
 
 }
